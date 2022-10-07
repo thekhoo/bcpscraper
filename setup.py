92e0d009ebd95b181b39e2658480d7b1b2743328
thekhoo <khoojinnwei@gmail.com>
2022-10-07 21:27:55 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = "Blackstone's Criminal Practice 2022 PDF Scraper"
+LONG_DESCRIPTION = "A program that scrapes the sections and subsections indicated by the user from Blackstone's Criminal Practice 2022 and puts it into a Word Document (.docx)."
+
+# Setting up
+setup(
+    name="bcpscraper",
+    version=VERSION,
+    author="Christopher Khoo Jinn Wei",
+    author_email="<khoojinnwei@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['docx','lxml','Pillow','PyPDF2','typing_extensions'],
+    keywords=['python',"Blackstone's Criminal Practice 2022",'law','law document','lexisnexis','lexislibrary'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Legal Industry",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Education"
+    ]
+)
